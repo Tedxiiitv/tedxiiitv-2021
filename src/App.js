b3bedafc574058d24cdb6088198d7af44bf25201
kalashshah <81062983+kalashshah@users.noreply.github.com>
2021-09-05 01:29:34 +0530

    Added Timer Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import "./App.css";
-import ComingSoon from "./Components/ComingSoon/ComingSoon"
+import ComingSoon from "./Components/ComingSoon/ComingSoon";
+import Timer from "./Components/Timer/Timer";
 
 function App() {
   return (
-    <div
-      className="App"
-      style={{ width: "100vw", height: "100vh", backgroundImage: `url("images/tedxbackground.png")`}}
-    >
-      <ComingSoon />
-    </div>
+    <>
+      <div
+        className="App"
+        style={{
+          width: "100wh",
+          height: "100vh",
+          backgroundImage: `url("images/tedxbackground.png")`,
+        }}
+      >
+        <ComingSoon />
+      </div>
+      <Timer />
+    </>
   );
 }
 
