ad9d932eae89d7a4950ebabc79b9d2f7b444f9b3
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2021-09-07 00:01:18 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ function App() {
   return (
     <div
       className="App"
-      style={{ width: "100vw", height: "100vh", background: "#000000" }}
+      style={{ width: "100vw", height: "100vh" }}
     >
-      <div style={{ height: "10%" }}></div>
+      {/* <div style={{ height: "10%" }}></div>
       <div style={{ height: "80%" }}>
         <LazyImage src={`images/tedxbackground.png`} />
       </div>
-      <div style={{ height: "10%" }}></div>
+      <div style={{ height: "10%" }}></div> */}
     </div>
   );
 }
