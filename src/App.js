d012fa039a8e5ab2087d9a6bafe7fe6dd406295d
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2021-09-07 00:10:37 +0530

    background set

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
+import React from "react";
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-    </Router>
+    <React.Fragment>
+      <div
+        style={{
+          backgroundImage: `url("images/tedxbackground.png")`,
+          height: "100vh",
+          width: "100vw",
+          position: "fixed",
+          top: "0",
+          left: "0",
+          zIndex: -1,
+        }}
+      ></div>
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+      </Router>
+    </React.Fragment>
   );
 }
 
