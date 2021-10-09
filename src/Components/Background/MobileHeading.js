6845a4c05460512534a56f66da1a369bf0dc5663
Madhur_Gupta <madleads2001@gmail.com>
2021-10-10 01:56:08 +0530

    Adds Mobile Background component

---
+++ src/Components/Background/MobileHeading.js
@@ -0,0 +1,10 @@
+import "./MobileHeading.css";
+const MobileHeading = () => {
+    return(
+        <div className="mobile-background">
+            THE NEXT WAVE
+        </div>
+    );
+}
+
+export default MobileHeading;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,7 +5,7 @@ import Footer from "../Footer/Footer";
 function Home() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
       <Footer />
     </div>
   );
