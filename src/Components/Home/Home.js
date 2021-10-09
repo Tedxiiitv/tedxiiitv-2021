aaa61555309ab766471d7f5f379862ca931ee12c
Kalash Shah <81062983+kalashshah@users.noreply.github.com>
2021-10-10 00:33:43 +0530

    Add Map and YouTube Invite component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import Invite from "../Invite/Invite";
 import Footer from "../Footer/Footer";
+
 
 function Home() {
   return (
     <div>
       <Navbar/>
+      <Invite />
       <Footer />
     </div>
   );
