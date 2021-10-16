a39bc406328dc8bcab5a9f20a484d579202c9370
Kalash Shah <81062983+kalashshah@users.noreply.github.com>
2021-10-16 16:05:23 +0530

    Make Landing page responsive and add links

--- src/Components/Social/Social.js
+++ src/Components/Social/Social.js
@@ -5,16 +5,16 @@ import './Social.css'
 function Social() {
   return (
     <div className='sociallogos fa fa-lg'>
-      <a className='socialmedialink' href='!#' target='_blank'>
+      <a className='socialmedialink' href='https://twitter.com/tedxiiitv' target='_blank'>
         <i className='fa fa-twitter icon'></i>
       </a>
-      <a className='socialmedialink' href='!#' target='_blank'>
+      <a className='socialmedialink' href='https://www.instagram.com/tedxiiitv' target='_blank'>
         <i className='fa fa-instagram'></i>
       </a>
-      <a className='socialmedialink' href='!#' target='_blank'>
+      <a className='socialmedialink' href='https://www.youtube.com/channel/UC4mwsjxwalruq9tga_3a92g' target='_blank'>
         <i className='fa fa-youtube-play'></i>
       </a>
-      <a className='socialmedialink' href='!#' target='_blank'>
+      <a className='socialmedialink' href='https://www.facebook.com/' target='_blank'>
         <i className='fa fa-facebook'></i>
       </a>
     </div>
