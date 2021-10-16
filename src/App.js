42488aac3e3bd08962b81ed7a9587dc2ede993eb
Prathmesh Khandelwal <201952225@iiitvadodara.ac.in>
2021-10-16 22:52:28 +0530

    feat/speaker page added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import './App.css'
-import Home from './Components/Home/Home'
-import Team from './Components/Team/Team'
-import About from './Components/About/About'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Team from "./Components/Team/Team";
+import About from "./Components/About/About";
+import Speaker from "./Components//Speaker/Speaker";
 
 function App() {
   return (
     <React.Fragment>
       <Router>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/team' component={Team} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/team" component={Team} />
+        <Route exact path="/speakers" component={Speaker} />
       </Router>
     </React.Fragment>
-  )
+  );
 }
 
-export default App
+export default App;
